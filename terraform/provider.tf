5e0070d4d98cf285bb45907c87d1fad306dc7078
balapraveen207 <85236865+balapraveen207@users.noreply.github.com>
2025-05-08 21:49:25 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket"
-    key            = "web-app/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
+provider "aws" {
+  region = var.aws_region
 }
